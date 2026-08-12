5272e4c728008952314ebcfa3b6c256ab387c00c
Glen <glendonnalpasan1@gmail.com>
2026-08-12 13:09:15 +0800

    Add Area Offices management: create index view and update routes

---
+++ resources/views/components/area-office/⚡index.blade.php
@@ -0,0 +1,243 @@
+<?php
+
+use Livewire\Component;
+use Flux\Flux;
+use App\Models\AreaOffices;
+use Livewire\WithPagination;
+
+new class extends Component {
+
+    use WithPagination;
+
+    public $area_office;
+    public $area_code;
+    public $deleteId;
+
+    public function render()
+    {
+        $areaOffices = AreaOffices::paginate(8);
+
+        return view('components.area-office.⚡index', [
+            'areaOffices' => $areaOffices,
+        ]);
+    }
+
+    public function save()
+    {
+        // Validate the input
+        $this->validate([
+            'area_office' => 'required|unique:area_offices,area_office|string|max:255',
+            'area_code' => 'nullable|unique:area_offices,area_code|string|max:255',
+        ]);
+
+        AreaOffices::create([
+            'area_office' => $this->area_office,
+            'area_code' => $this->area_code,
+        ]);
+
+        Flux::toast(heading: 'Asset type added', text: 'The new asset type has been added.', variant: 'success');
+
+        $this->reset();
+
+        flux::modal('create-asset-type')->close();
+    }
+
+    public function delete()
+    {
+        AreaOffices::findOrFail($this->deleteId)->delete();
+
+        $this->deleteId = null;
+
+        Flux::modal('delete-asset-type')->close();
+
+        Flux::toast(
+            heading: 'Success',
+            text: 'Extension Office deleted successfully.',
+            variant: 'success'
+        );
+    }
+
+    public function confirmDelete($id)
+    {
+        $this->deleteId = $id;
+
+        Flux::modal('delete-asset-type')->show();
+    }
+};
+?>
+
+<section class="w-full">
+    @include('partials.settings-heading')
+
+    <x-settings.layout :heading="__('Area Office')" :subheading="__('Manage Area offices.')">
+        <div class="space-y-6">
+
+            {{-- Add Asset Type --}}
+            <flux:card class="p-6">
+                <div class="flex items-end gap-4">
+                    <form wire:submit="save" class="flex flex-col md:flex-row items-end gap-4 w-full">
+
+                        <div class="flex-1 w-full">
+                            <flux:input
+                                wire:model.defer="area_office"
+                                label="Area Office"
+                                placeholder="e.g. Area Office B, Resort A, Building C, etc..."
+                            />
+                        </div>
+
+                        <div class="flex-1 w-full">
+                            <flux:input
+                                wire:model.defer="area_code"
+                                label="Area Code"
+                                placeholder="enter area code"
+                            />
+                        </div>
+
+                        <flux:button
+                            type="submit"
+                            variant="primary"
+                            icon="plus"
+                            class="cursor-pointer"
+                        >
+                            Add Area Office
+                        </flux:button>
+
+                    </form>
+                </div>
+            </flux:card>
+
+            {{-- Existing Asset Types --}}
+            <flux:card>
+
+                <div class="flex items-center justify-between px-6 py-4 border-b">
+
+                    <div>
+                        <flux:heading size="lg">
+                            Existing Offices
+                        </flux:heading>
+
+                        <flux:text class="mt-1">
+                            Manage the assets to the assigned available offices or properties.
+                        </flux:text>
+                    </div>
+
+                    <flux:input icon="magnifying-glass" placeholder="Search..." class="max-w-xs" />
+
+                </div>
+
+                <div class="overflow-x-auto">
+
+                <table :paginate="$areaOffices" class="w-full">
+
+                    <thead class="bg-zinc-50 dark:bg-zinc-800">
+
+                        <tr>
+
+                            <th class="px-6 py-3 text-sm font-medium text-center">
+                                Area Office
+                            </th>
+
+                            <th class="px-6 py-3 text-sm font-medium text-center w-40">
+                                Area Code
+                            </th>
+
+                            <th class="px-6 py-3 text-sm font-medium text-center w-40">
+                                Actions
+                            </th>
+
+                        </tr>
+
+                    </thead>
+
+                    <tbody class="divide-y divide-zinc-200 dark:divide-zinc-800">
+
+                        @foreach ($areaOffices as $type)
+                            <tr wire:key="asset-type-{{ $type->id }}">
+
+                                <td class="px-7 py-5 text-center">
+                                    {{ $type->area_office }}
+                                </td>
+
+                                <td class="px-7 py-5 text-center">
+                                    {{ $type->area_code ?? '-'}}
+                                </td>
+
+                                <td class="px-7 py-4">
+                                    <div class="flex justify-center gap-2">
+
+                                        <flux:button
+                                            size="sm"
+                                            class="cursor-pointer"
+                                            variant="ghost"
+                                            icon="pencil-square">
+                                            Edit
+                                        </flux:button>
+
+                                        <flux:button
+                                            size="sm"
+                                            class="cursor-pointer"
+                                            variant="danger"
+                                            icon="trash"
+                                            wire:click="confirmDelete({{ $type->id }})">
+                                            Delete
+                                        </flux:button>
+
+                                    </div>
+                                </td>
+
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+
+                </table>
+
+                    <!-- $orders = Order::paginate(5) -->
+                    <flux:pagination class="cursor-pointer" :paginator="$areaOffices" />
+
+                </div>
+
+            </flux:card>
+
+        </div>
+
+    </x-settings.layout>
+
+    <!---- Confirmation Modal when deleting ---->
+    <flux:modal name="delete-asset-type" class="max-w-md">
+    <div class="space-y-6">
+
+        <div>
+            <flux:heading size="lg">
+                Delete Asset Type
+            </flux:heading>
+
+            <flux:text class="mt-2">
+                Are you sure you want to delete this asset type? This action
+                cannot be undone.
+            </flux:text>
+        </div>
+
+        <div class="flex justify-end gap-2">
+            <flux:button
+                variant="ghost"
+                x-on:click="$flux.modal('delete-asset-type').close()"
+                class="cursor-pointer"
+            >
+                Cancel
+            </flux:button>
+
+            <flux:button
+                variant="danger"
+                icon="trash"
+                wire:click="delete"
+                class="cursor-pointer"
+            >
+                Delete
+            </flux:button>
+        </div>
+
+    </div>
+    </flux:modal>
+
+</section>
